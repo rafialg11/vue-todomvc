8e28ea6e03fc8ac2efb8b3d0b91a48b51b4f52dd
rafialg11 <53641104+rafialg11@users.noreply.github.com>
2021-03-04 17:13:58 +0700

    rapihin komen

--- app.js
+++ app.js
@@ -5,7 +5,8 @@
  **/
 
 /**
- * The todos are the "model" of this application. In a real-world example, they would probably be loaded asynchronously, though they could also be injected via server-side rendering.
+ * The todos are the "model" of this application. In a real-world example, they would probably be
+ *  loaded asynchronously, though they could also be injected via server-side rendering.
  * Changing these todos will change their appearance in the application.
  */
 let todos = [{
@@ -20,23 +21,29 @@ let todos = [{
 }];
 
 /**
- * All Vue applications are initialized with a `new Vue()` method call. The object that is passed to the method call is the configuration for our whole application.
+ * All Vue applications are initialized with a `new Vue()` method call. The object that is passed 
+ * to the method call is the configuration for our whole application.
  */
 new Vue({
     /**
-     * The "el" option indicates which HTML element the application should initially load, into. If you look at `index.html`, you will notice a div tag with the ID #app. This initial load process is also known as "bootstrapping."
+     * The "el" option indicates which HTML element the application should initially load, into.
+     *  If you look at `index.html`, you will notice a div tag with the ID #app. This initial 
+     * load process is also known as "bootstrapping."
      */
     el:`#app`,
 
     /**
      * The template for the application defines the basic structure of its HTML.
-     * There are many ways to define a template in Vue, but this example uses HTML templates. If you look at the Index file, you will see an `x-template` tag with this ID. Changing the contents of that tag will change the way the application looks.
+     * There are many ways to define a template in Vue, but this example uses HTML templates. If you 
+     * look at the Index file, you will see an `x-template` tag with this ID. Changing the contents 
+     * of that tag will change the way the application looks.
      * Other ways to define a template include using JSX or just a JavaScript string.
      */
     template:"#app-template",
 
     /**
-     * The data method is called by the component, and the values returned become the data model for that component.
+     * The data method is called by the component, and the values returned become the data 
+     * model for that component.
      * The todos being passed here are defined at the top of this file.
      */
     data:()=>(
@@ -63,7 +70,8 @@ new Vue({
 
         /**
          * A boolean indicating if the submit button should be disabled, based on the content of the input box.
-         * This method could be upgraded to take into accounts other things - like if the text input is valid based on the output of some validator.
+         * This method could be upgraded to take into accounts other things - like if the text input is 
+         * valid based on the output of some validator.
          */
         submitIsDisabled(){
             return this.text == "";
@@ -71,13 +79,15 @@ new Vue({
     },
 
     /**
-     * The methods property of a Vue app contains functions, which are usually called from the template, though they can be called from elsewhere in the script, too.
+     * The methods property of a Vue app contains functions, which are usually called from the template, 
+     * though they can be called from elsewhere in the script, too.
      * If you look in the template, you will find a form which calls this "addTodo()" method.
      * Methods are the preferred way of doing an action that changes the data model.
      */
     methods:{
         /**
-         * addTodo() takes whatever is in the text input box, and makes it into a list element. There is no need to call a `render()` method, as Vue can determine on its own what to redraw.
+         * addTodo() takes whatever is in the text input box, and makes it into a list element. 
+         * There is no need to call a `render()` method, as Vue can determine on its own what to redraw.
          */
         addTodo(){
             todos.push({
